328c71f97a9a69d12e948b14252d91d4cfe70eb6
김상수 <151427064+raminkim@users.noreply.github.com>
2025-05-04 00:36:38 +0900

    CalculatorLv1 수정

--- src/calculator1/CalculatorLv1.java
+++ src/calculator1/CalculatorLv1.java
@@ -2,24 +2,6 @@ package calculator1;
 
 import java.util.Scanner;
 
-/*
-while 반복문 활용한 간단한 사칙 연산 계산기
-
-가정:
- - 입력받는 첫 번째, 두 번째 숫자는 int 타입으로 가정한다.
-
-입력 순서:
- 1. 첫 번째 숫자 (int)
- 2. 두 번째 숫자 (int)
- 3. 연산 기호 (+, -, *, / 중 하나)
-
-종료 조건
- - "더 계산하시겠습니까? (exit 입력 시 종료)" 출력 직후에 "exit" 문자열 입력 시 반복문이 종료된다.
-
-예외 처리
- - 연산자가 +, -, *, /가 아닌 다른 기호나 문자를 입력 시, "적절한 연산 기호가 아닙니다"라고 출력
- - 0으로 나누기 시도 시 (= 연산자가 /이며 두 번째 숫자가 0일 경우), "분모에 0이 입력될 수 없습니다"라고 출력
- */
 public class CalculatorLv1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
